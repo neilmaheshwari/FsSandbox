a1c000b25a1da4fc1c83a710bd175f4efb63329a
Neil <neil@beyondgames.co>
2015-09-13 16:25:43 -0700

    Initial commit.

---
+++ ZMQ/AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace ZMQ
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("ZMQ")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Neil")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
