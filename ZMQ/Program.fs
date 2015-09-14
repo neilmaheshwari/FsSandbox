9f0fe52facae36e58bad897739b9ef482ab46258
Neil <neil@beyondgames.co>
2015-09-13 19:17:00 -0700

    Adding rereq example

--- ZMQ/Program.fs
+++ ZMQ/Program.fs
@@ -5,6 +5,16 @@ open fszmq
 
 [<EntryPoint>]
 let main argv = 
+//
+//    let pipe = "inproc://example"
+//
+//    let context = new Context ()
+//    let sink = Context.router context
+//    Socket.bind sink pipe
+//    let server = Context.rep context
+
+    rereq.main ()
+
     printfn "%A" argv
     0 // return an integer exit code
 
